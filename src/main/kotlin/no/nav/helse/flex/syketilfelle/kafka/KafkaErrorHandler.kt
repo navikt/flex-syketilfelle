e4f0ec209168d06cae8a6c0c8005ec65123ab307
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-12-23 14:08:50 +0100

    Handle batch

--- src/main/kotlin/no/nav/helse/flex/syketilfelle/kafka/KafkaErrorHandler.kt
+++ src/main/kotlin/no/nav/helse/flex/syketilfelle/kafka/KafkaErrorHandler.kt
@@ -3,9 +3,11 @@ package no.nav.helse.flex.syketilfelle.kafka
 import no.nav.helse.flex.syketilfelle.logger
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.springframework.kafka.listener.*
 import org.springframework.stereotype.Component
 import org.springframework.util.backoff.ExponentialBackOff
+import java.lang.Exception
 
 @Component
 class KafkaErrorHandler : DefaultErrorHandler(
@@ -17,7 +19,7 @@ class KafkaErrorHandler : DefaultErrorHandler(
     val log = logger()
 
     override fun handleRemaining(
-        thrownException: java.lang.Exception,
+        thrownException: Exception,
         records: MutableList<ConsumerRecord<*, *>>,
         consumer: Consumer<*, *>,
         container: MessageListenerContainer
@@ -32,5 +34,24 @@ class KafkaErrorHandler : DefaultErrorHandler(
             log.error("Feil i listener uten noen records", thrownException)
         }
         super.handleRemaining(thrownException, records, consumer, container)
+    }
+
+    override fun handleBatch(
+        thrownException: Exception,
+        records: ConsumerRecords<*, *>,
+        consumer: Consumer<*, *>,
+        container: MessageListenerContainer,
+        invokeListener: Runnable
+    ) {
+        records.forEach { record ->
+            log.error(
+                "Feil i prossessering av record med offset: ${record.offset()}, partition: ${record.partition()} på topic ${record.topic()}",
+                thrownException
+            )
+        }
+        if (records.isEmpty()) {
+            log.error("Feil i listener uten noen records", thrownException)
+        }
+        super.handleBatch(thrownException, records, consumer, container, invokeListener)
     }
 }
