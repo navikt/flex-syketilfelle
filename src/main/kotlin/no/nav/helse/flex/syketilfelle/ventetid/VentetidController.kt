7f97e8aab6ad4bbd9a253deaedb659af7b8b6daf
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-01-12 14:49:23 +0100

    Ventetid API for sykmelding frontend

--- src/main/kotlin/no/nav/helse/flex/syketilfelle/ventetid/VentetidController.kt
+++ src/main/kotlin/no/nav/helse/flex/syketilfelle/ventetid/VentetidController.kt
@@ -4,7 +4,10 @@ import no.nav.helse.flex.syketilfelle.client.pdl.PdlClient
 import no.nav.helse.flex.syketilfelle.clientidvalidation.ClientIdValidation
 import no.nav.helse.flex.syketilfelle.clientidvalidation.ClientIdValidation.NamespaceAndApp
 import no.nav.helse.flex.syketilfelle.identer.MedPdlClient
+import no.nav.helse.flex.syketilfelle.identer.fnrFraLoginservicetoken
+import no.nav.helse.flex.syketilfelle.sykeforloep.SykeforloepUtregner
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.*
 class VentetidController(
     private val clientIdValidation: ClientIdValidation,
     private val ventetidUtregner: VentetidUtregner,
+    private val tokenValidationContextHolder: TokenValidationContextHolder,
+    private val sykeforloepUtregner: SykeforloepUtregner,
     override val pdlClient: PdlClient,
 ) : MedPdlClient {
 
@@ -47,5 +52,26 @@ class VentetidController(
             erUtenforVentetidRequest = erUtenforVentetidRequest,
             fnrs = alleFnrs,
         )
+    }
+
+    @GetMapping(
+        "/api/bruker/v1/ventetid/{sykmeldingId}/erUtenforVentetid",
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    @ResponseBody
+    @ProtectedWithClaims(issuer = "loginservice", claimMap = ["acr=Level4"])
+    fun erUtenforVentetid(@PathVariable("sykmeldingId") sykmeldingId: String): ErUtenforVentetidResponse {
+        val fnr = tokenValidationContextHolder.fnrFraLoginservicetoken()
+        val fnrs = pdlClient.hentFolkeregisterIdenter(fnr)
+
+        val utenforVentetid =
+            ventetidUtregner.beregnOmSykmeldingErUtenforVentetid(sykmeldingId, fnrs, ErUtenforVentetidRequest())
+
+        val sykeforloep = sykeforloepUtregner.hentSykeforloep(fnrs, inkluderPapirsykmelding = false)
+        val oppfolgingsdato = sykeforloep
+            .find { it.sykmeldinger.any { sm -> sm.id == sykmeldingId } }
+            ?.oppfolgingsdato
+
+        return ErUtenforVentetidResponse(utenforVentetid, oppfolgingsdato)
     }
 }
