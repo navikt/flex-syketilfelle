8c2dbde7ee129620603399545a53ad241fcb76d5
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-02-18 11:23:47 +0100

    Hent fnr fra "pid" i tillegg til "sub"

--- src/main/kotlin/no/nav/helse/flex/syketilfelle/identer/fnrFraLoginservicetoken.kt
+++ src/main/kotlin/no/nav/helse/flex/syketilfelle/identer/fnrFraLoginservicetoken.kt
@@ -3,6 +3,6 @@ package no.nav.helse.flex.syketilfelle.identer
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 
 fun TokenValidationContextHolder.fnrFraLoginservicetoken(): String {
-    val context = this.tokenValidationContext
-    return context.getClaims("loginservice").subject
+    val claims = this.tokenValidationContext.getClaims("loginservice")
+    return claims.getStringClaim("pid") ?: claims.subject
 }
