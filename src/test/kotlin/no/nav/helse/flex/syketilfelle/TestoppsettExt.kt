65ba7ab58e542cb5e5ca842e845d74caf37f70b4
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-08-12 09:49:33 +0200

    Sjekk bare gjeldende orgnummer i ag periode utregning

---
+++ src/test/kotlin/no/nav/helse/flex/syketilfelle/TestoppsettExt.kt
@@ -0,0 +1,96 @@
+package no.nav.helse.flex.syketilfelle
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
+import no.nav.helse.flex.syketilfelle.arbeidsgiverperiode.domain.Arbeidsgiverperiode
+import no.nav.helse.flex.syketilfelle.sykmelding.domain.MottattSykmeldingKafkaMessage
+import no.nav.helse.flex.syketilfelle.sykmelding.domain.SykmeldingKafkaMessage
+import no.nav.syfo.model.sykmelding.arbeidsgiver.ArbeidsgiverSykmelding
+import no.nav.syfo.model.sykmeldingstatus.ArbeidsgiverStatusDTO
+import no.nav.syfo.model.sykmeldingstatus.KafkaMetadataDTO
+import no.nav.syfo.model.sykmeldingstatus.STATUS_SENDT
+import no.nav.syfo.model.sykmeldingstatus.SykmeldingStatusKafkaEventDTO
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+
+fun Testoppsett.kallArbeidsgiverperiodeApi(
+    soknad: SykepengesoknadDTO,
+    expectNoContent: Boolean = false,
+    forelopig: Boolean = true,
+    fnr: String,
+): Arbeidsgiverperiode? {
+    val result = mockMvc.perform(
+        MockMvcRequestBuilders.post("/api/v1/arbeidsgiverperiode")
+            .header("Authorization", "Bearer ${server.azureToken(subject = "sykepengesoknad-backend-client-id")}")
+            .header("fnr", fnr)
+            .header("forelopig", forelopig.toString())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(soknad))
+    )
+        .andExpect(MockMvcResultMatchers.status().is2xxSuccessful)
+        .andExpect(
+            if (expectNoContent) MockMvcResultMatchers.status().isNoContent else
+                MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+        )
+        .andReturn()
+    return result.response.contentAsString.takeIf { it.isNotBlank() }
+        ?.let { objectMapper.readValue(it) }
+}
+
+fun Testoppsett.opprettSendtSykmelding(
+    sykmelding: ArbeidsgiverSykmelding,
+    fnr: String,
+    orgnummer: String? = null
+): String {
+
+    val kafkaMessage = opprettSykmeldingKafkaMessage(sykmelding, fnr, orgnummer)
+    producerPåSendtBekreftetTopic(kafkaMessage)
+    return sykmelding.id
+}
+
+fun Testoppsett.opprettMottattSykmelding(sykmelding: ArbeidsgiverSykmelding, fnr: String): String {
+
+    val kafkaMessage = opprettSykmeldingKafkaMessage(sykmelding = sykmelding, fnr = fnr)
+    val apenSykmeldingKafkaMessage = MottattSykmeldingKafkaMessage(
+        sykmelding = kafkaMessage.sykmelding,
+        kafkaMetadata = kafkaMessage.kafkaMetadata
+    )
+    producerPåMottattTopic(apenSykmeldingKafkaMessage)
+    return sykmelding.id
+}
+
+private fun opprettSykmeldingKafkaMessage(
+    sykmelding: ArbeidsgiverSykmelding,
+    fnr: String,
+    orgnummer: String? = null
+): SykmeldingKafkaMessage {
+
+    val kafkaMetadata = KafkaMetadataDTO(
+        sykmeldingId = sykmelding.id,
+        fnr = fnr,
+        timestamp = OffsetDateTime.now(),
+        source = "Denne testen"
+    )
+
+    val arbeidsgiver = if (orgnummer != null) {
+        ArbeidsgiverStatusDTO(orgnummer = orgnummer, orgNavn = "$orgnummer sitt navn")
+    } else {
+        null
+    }
+
+    val event = SykmeldingStatusKafkaEventDTO(
+        sykmeldingId = sykmelding.id,
+        timestamp = OffsetDateTime.now(),
+        statusEvent = STATUS_SENDT,
+        arbeidsgiver = arbeidsgiver,
+        sporsmals = emptyList()
+    )
+    return SykmeldingKafkaMessage(
+        sykmelding = sykmelding,
+        kafkaMetadata = kafkaMetadata,
+        event = event.copy(timestamp = OffsetDateTime.of(2020, 6, 20, 6, 34, 4, 0, ZoneOffset.UTC))
+    )
+}
---
+++ src/test/kotlin/no/nav/helse/flex/syketilfelle/arbeidsgiverperiode/ArbeidsgiverperiodeMedFlereArbeidsgivereTest.kt
@@ -0,0 +1,87 @@
+package no.nav.helse.flex.syketilfelle.arbeidsgiverperiode
+
+import no.nav.helse.flex.sykepengesoknad.kafka.ArbeidsgiverDTO
+import no.nav.helse.flex.sykepengesoknad.kafka.SoknadsstatusDTO
+import no.nav.helse.flex.sykepengesoknad.kafka.SoknadstypeDTO
+import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
+import no.nav.helse.flex.syketilfelle.Testoppsett
+import no.nav.helse.flex.syketilfelle.kallArbeidsgiverperiodeApi
+import no.nav.helse.flex.syketilfelle.opprettMottattSykmelding
+import no.nav.helse.flex.syketilfelle.opprettSendtSykmelding
+import no.nav.helse.flex.syketilfelle.sykmelding.skapArbeidsgiverSykmelding
+import no.nav.helse.flex.syketilfelle.ventPåRecords
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.Duration
+import java.time.LocalDate
+import java.util.*
+
+class ArbeidsgiverperiodeMedFlereArbeidsgivereTest : Testoppsett() {
+
+    private final val fnr = "11111555555"
+    private final val orgnr1 = "888888888"
+    private final val orgnr2 = "999999999"
+    private final val basisDato = LocalDate.of(2022, 6, 30)
+
+    @BeforeEach
+    @AfterEach
+    fun setUp() {
+        syketilfellebitRepository.deleteAll()
+    }
+
+    @Test
+    fun `arbrbeidsgiverperioden regnes ut per arbeidsgiver`() {
+
+        val sykmelding = skapArbeidsgiverSykmelding(fom = basisDato.minusDays(12), tom = basisDato)
+        val sykmelding2 = skapArbeidsgiverSykmelding(fom = basisDato.minusDays(24), tom = basisDato)
+
+        opprettMottattSykmelding(sykmelding = sykmelding, fnr = fnr)
+        opprettMottattSykmelding(sykmelding = sykmelding2, fnr = fnr)
+        opprettSendtSykmelding(sykmelding = sykmelding, fnr = fnr, orgnummer = orgnr1)
+        opprettSendtSykmelding(sykmelding = sykmelding2, fnr = fnr, orgnummer = orgnr2)
+
+        val soknad1 = SykepengesoknadDTO(
+            id = UUID.randomUUID().toString(),
+            arbeidsgiver = ArbeidsgiverDTO(navn = "navn", orgnummer = orgnr1),
+            fravar = emptyList(),
+            andreInntektskilder = emptyList(),
+            fom = basisDato.minusDays(12),
+            tom = basisDato,
+            arbeidGjenopptatt = null,
+            egenmeldinger = emptyList(),
+            fnr = fnr,
+            status = SoknadsstatusDTO.SENDT,
+            sykmeldingId = sykmelding.id,
+            type = SoknadstypeDTO.ARBEIDSTAKERE
+        )
+
+        val ag1 = kallArbeidsgiverperiodeApi(soknad = soknad1, forelopig = false, fnr = fnr)!!
+        assertThat(ag1.oppbruktArbeidsgiverperiode).isEqualTo(false)
+        assertThat(ag1.arbeidsgiverPeriode.fom).isEqualTo(soknad1.fom)
+        assertThat(ag1.arbeidsgiverPeriode.tom).isEqualTo(soknad1.tom)
+
+        val soknad2 = SykepengesoknadDTO(
+            id = UUID.randomUUID().toString(),
+            arbeidsgiver = ArbeidsgiverDTO(navn = "navn", orgnummer = orgnr2),
+            fravar = emptyList(),
+            andreInntektskilder = emptyList(),
+            fom = basisDato.minusDays(24),
+            tom = basisDato,
+            arbeidGjenopptatt = null,
+            egenmeldinger = emptyList(),
+            fnr = fnr,
+            status = SoknadsstatusDTO.SENDT,
+            sykmeldingId = sykmelding2.id,
+            type = SoknadstypeDTO.ARBEIDSTAKERE
+        )
+
+        val ag2 = kallArbeidsgiverperiodeApi(soknad = soknad2, forelopig = false, fnr = fnr)!!
+        assertThat(ag2.oppbruktArbeidsgiverperiode).isEqualTo(true)
+        assertThat(ag2.arbeidsgiverPeriode.fom).isEqualTo(soknad2.fom)
+        assertThat(ag2.arbeidsgiverPeriode.tom).isEqualTo(soknad2.fom!!.plusDays(15))
+
+        juridiskVurderingKafkaConsumer.ventPåRecords(antall = 2, duration = Duration.ofSeconds(5))
+    }
+}
