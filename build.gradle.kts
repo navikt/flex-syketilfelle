cf9220b7e2518af157c15b2ba7c8ae593842b839
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-03 10:31:23 +0200

    Bumper kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     id("org.springframework.boot") version "2.7.4"
     id("io.spring.dependency-management") version "1.0.14.RELEASE"
-    kotlin("plugin.spring") version "1.7.10"
-    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.20"
+    kotlin("jvm") version "1.7.20"
 }
 
 group = "no.nav.helse.flex"
