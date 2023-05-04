56dc010590d96c575caea24e58795f3e3d1ce273
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-05-04 10:29:34 +0200

    enable tombstone (#181)

--- src/main/kotlin/no/nav/helse/flex/syketilfelle/kafkaprodusering/TombstoneCronJob.kt
+++ src/main/kotlin/no/nav/helse/flex/syketilfelle/kafkaprodusering/TombstoneCronJob.kt
@@ -1,13 +1,11 @@
 package no.nav.helse.flex.syketilfelle.kafkaprodusering
 
 import no.nav.helse.flex.syketilfelle.logger
-import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
 
 @Component
-@Profile("tombstoneprodusering")
 class TombstoneCronJob(
     val tombsstoneProduseringJob: TombsstoneProduseringJob,
     val leaderElection: LeaderElection
