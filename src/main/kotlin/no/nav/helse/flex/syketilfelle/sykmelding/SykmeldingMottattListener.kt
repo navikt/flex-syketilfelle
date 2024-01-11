9650ed89e5e4c19d56202e75929e11e02e8c61c4
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2024-01-11 08:42:22 +0100

    Reset og commit offset på sykmelding topic (#308)

--- src/main/kotlin/no/nav/helse/flex/syketilfelle/sykmelding/SykmeldingMottattListener.kt
+++ src/main/kotlin/no/nav/helse/flex/syketilfelle/sykmelding/SykmeldingMottattListener.kt
@@ -1,19 +1,27 @@
 package no.nav.helse.flex.syketilfelle.sykmelding
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.syketilfelle.extensions.osloZone
+import no.nav.helse.flex.syketilfelle.logger
 import no.nav.helse.flex.syketilfelle.objectMapper
 import no.nav.helse.flex.syketilfelle.sykmelding.domain.MottattSykmeldingKafkaMessage
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 
 const val SYKMELDINGMOTTATT_TOPIC = "teamsykmelding." + "syfo-mottatt-sykmelding"
 
 @Component
 class AivenSykmeldingMottattListener(
     private val sykmeldingLagring: SykmeldingLagring,
-) {
+) : ConsumerSeekAware {
+    private val log = logger()
+    private val startTimestamp = LocalDate.of(2024, 1, 8).atStartOfDay(osloZone).withHour(17).withMinute(20).toInstant().toEpochMilli()
+
     @KafkaListener(
         topics = [SYKMELDINGMOTTATT_TOPIC],
         id = "sykmelding-mottatt",
@@ -29,6 +37,15 @@ class AivenSykmeldingMottattListener(
         sykmeldingLagring.handterMottattSykmelding(cr.key(), sykmeldingDTO, cr.topic())
 
         acknowledgment.acknowledge()
+    }
+
+    override fun onPartitionsAssigned(
+        assignments: Map<TopicPartition, Long>,
+        callback: ConsumerSeekAware.ConsumerSeekCallback,
+    ) {
+        // Seek all the assigned partition to a certain offset
+        callback.seekToTimestamp(assignments.keys, startTimestamp)
+        log.info("Ferdig med seekToTimestamp fra klassen ${this.javaClass.simpleName}")
     }
 
     fun String.tilSykmeldingDTO(): MottattSykmeldingKafkaMessage = objectMapper.readValue(this)
--- src/main/kotlin/no/nav/helse/flex/syketilfelle/sykmelding/SykmeldingSendtBekreftetListener.kt
+++ src/main/kotlin/no/nav/helse/flex/syketilfelle/sykmelding/SykmeldingSendtBekreftetListener.kt
@@ -1,12 +1,17 @@
 package no.nav.helse.flex.syketilfelle.sykmelding
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.syketilfelle.extensions.osloZone
+import no.nav.helse.flex.syketilfelle.logger
 import no.nav.helse.flex.syketilfelle.objectMapper
 import no.nav.helse.flex.syketilfelle.sykmelding.domain.SykmeldingKafkaMessage
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 
 const val SYKMELDINGSENDT_TOPIC = "teamsykmelding." + "syfo-sendt-sykmelding"
 const val SYKMELDINGBEKREFTET_TOPIC = "teamsykmelding." + "syfo-bekreftet-sykmelding"
@@ -14,7 +19,10 @@ const val SYKMELDINGBEKREFTET_TOPIC = "teamsykmelding." + "syfo-bekreftet-sykmel
 @Component
 class SykmeldingSendtBekreftetListener(
     private val sykmeldingLagring: SykmeldingLagring,
-) {
+) : ConsumerSeekAware {
+    private val log = logger()
+    private val startTimestamp = LocalDate.of(2024, 1, 8).atStartOfDay(osloZone).withHour(17).withMinute(20).toInstant().toEpochMilli()
+
     @KafkaListener(
         topics = [SYKMELDINGSENDT_TOPIC],
         id = "sykmelding-sendt",
@@ -39,6 +47,15 @@ class SykmeldingSendtBekreftetListener(
         acknowledgment: Acknowledgment,
     ) {
         listen(cr, acknowledgment)
+    }
+
+    override fun onPartitionsAssigned(
+        assignments: Map<TopicPartition, Long>,
+        callback: ConsumerSeekAware.ConsumerSeekCallback,
+    ) {
+        // Seek all the assigned partition to a certain offset
+        callback.seekToTimestamp(assignments.keys, startTimestamp)
+        log.info("Ferdig med seekToTimestamp fra klassen ${this.javaClass.simpleName}")
     }
 
     private fun listen(
