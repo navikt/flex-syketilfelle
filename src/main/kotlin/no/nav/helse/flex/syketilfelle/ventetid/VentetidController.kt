011cd93548629f837cd4156e9777b31b4b4a4558
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2025-03-12 13:56:27 +0100

    Legger til støtte for flex-sykmeldinger-backend for /er-utenfor-ventetid (#469)

--- src/main/kotlin/no/nav/helse/flex/syketilfelle/ventetid/VentetidController.kt
+++ src/main/kotlin/no/nav/helse/flex/syketilfelle/ventetid/VentetidController.kt
@@ -26,6 +26,8 @@ class VentetidController(
     override val pdlClient: PdlClient,
     @Value("\${SYKMELDINGER_FRONTEND_CLIENT_ID}")
     val sykmeldingerFrontendClientId: String,
+    @Value("\${FLEX_SYKMELDINGER_BACKEND_CLIENT_ID}")
+    val flexSykmeldingerBackendClientId: String,
 ) : MedPdlClient {
     val log = logger()
 
@@ -97,7 +99,7 @@ class VentetidController(
         val context = tokenValidationContextHolder.getTokenValidationContext()
         val claims = context.getClaims("tokenx")
         val clientId = claims.getStringClaim("client_id")
-        if (clientId != sykmeldingerFrontendClientId) {
+        if (clientId !in listOf(sykmeldingerFrontendClientId, flexSykmeldingerBackendClientId)) {
             throw IngenTilgang("Uventet client id $clientId")
         }
 
