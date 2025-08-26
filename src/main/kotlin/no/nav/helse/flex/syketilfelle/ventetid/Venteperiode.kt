0a02e3fda6768f5057aaa85f6fa59d24d7bb8701
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-08-26 16:30:51 +0200

    Splitt filer for ventetid og venteperiode dataklasser

---
+++ src/main/kotlin/no/nav/helse/flex/syketilfelle/ventetid/Venteperiode.kt
@@ -0,0 +1,19 @@
+package no.nav.helse.flex.syketilfelle.ventetid
+
+import no.nav.helse.flex.syketilfelle.sykmelding.domain.SykmeldingKafkaMessage
+import java.time.LocalDate
+
+data class VenteperiodeRequest(
+    val tilleggsopplysninger: Tilleggsopplysninger? = null,
+    val sykmeldingKafkaMessage: SykmeldingKafkaMessage? = null,
+    val harForsikring: Boolean = false,
+)
+
+data class VenteperiodeResponse(
+    val venteperiode: Venteperiode?,
+)
+
+data class Venteperiode(
+    val fom: LocalDate,
+    val tom: LocalDate,
+)
